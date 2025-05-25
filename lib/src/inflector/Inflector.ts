0edfb5acffc431a97fa3b5c932be077b1fe1f883
Kyrylo <khris.private@gmail.com>
2025-05-25 19:26:11 +0300

    refactor: add missing jsdocs

--- lib/src/inflector/Inflector.ts
+++ lib/src/inflector/Inflector.ts
@@ -3,7 +3,6 @@ import { pluralize, singularize } from 'inflected';
 /**
  * Inflector is a singleton class that provides methods for singularizing and pluralizing words.
  * It uses the inflected library to perform the transformations.
- *
  * @example
  * const inflector = Inflector.instance;
  * const singular = inflector.singularize("books"); // "book"
@@ -14,7 +13,6 @@ import { pluralize, singularize } from 'inflected';
  *   singularize: (word) => word.replace(/s$/, ""),
  *   pluralize: (word) => word + "s"
  * });
- *
  */
 export default class Inflector {
   static #instance: Inflector;
@@ -26,6 +24,10 @@ export default class Inflector {
     this.pluralizeFn = pluralize;
   }
 
+  /**
+   * Returns the singleton instance of the Inflector.
+   * @returns The singleton instance of the Inflector.
+   */
   static get instance(): Inflector {
     if (!Inflector.#instance) {
       Inflector.#instance = new Inflector();
@@ -33,6 +35,12 @@ export default class Inflector {
     return Inflector.#instance;
   }
 
+  /**
+   * Registers a custom inflector.
+   * @param inflector - The inflector to register.
+   * @param inflector.singularize - The function to use for singularizing words.
+   * @param inflector.pluralize - The function to use for pluralizing words.
+   */
   register(inflector: {
     singularize: (word: string) => string;
     pluralize: (word: string) => string;
@@ -41,10 +49,20 @@ export default class Inflector {
     this.pluralizeFn = inflector.pluralize;
   }
 
+  /**
+   * Singularizes a word.
+   * @param word - The word to singularize.
+   * @returns The singularized word.
+   */
   singularize(word: string): string {
     return this.singularizeFn(word);
   }
 
+  /**
+   * Pluralizes a word.
+   * @param word - The word to pluralize.
+   * @returns The pluralized word.
+   */
   pluralize(word: string): string {
     return this.pluralizeFn(word);
   }
--- lib/src/utils/MirageError.ts
+++ lib/src/utils/MirageError.ts
@@ -1,3 +1,11 @@
+/**
+ * A custom error class for MirageJS.
+ * @augments Error
+ * @param message - The error message.
+ * @example
+ * const error = new MirageError('Something went wrong');
+ * console.log(error.message); // => '[Mirage]: Something went wrong'
+ */
 export class MirageError extends Error {
   constructor(message: string) {
     super(`[Mirage]: ${message}`);
