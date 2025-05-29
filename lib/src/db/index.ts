f55020c54d0600c33ab061a5d0cdeaec75498a75
Kyrylo <khris.private@gmail.com>
2025-05-29 13:13:49 +0300

    refactor(db): improve types and sort methods

--- lib/src/db/index.ts
+++ lib/src/db/index.ts
@@ -1,3 +1,12 @@
 export { default as DB, type DbOptions } from './DB';
-export { default as DbCollection, type DbCollectionOptions, type DbRecord } from './DbCollection';
-export { default as IdentityManager, type IdentityManagerConfig } from './IdentityManager';
+export {
+  default as DbCollection,
+  type DbCollectionOptions,
+  type DbRecord,
+  type DbRecordInput,
+} from './DbCollection';
+export {
+  default as IdentityManager,
+  type AllowedIdTypes,
+  type IdentityManagerConfig,
+} from './IdentityManager';
