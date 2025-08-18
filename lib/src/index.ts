02b6a551c65357e74d5e7f9ed79fc979935847ff
Kyrylo <khris.private@gmail.com>
2025-08-18 17:59:40 +0300

    feat: update index entry file

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,4 +1,37 @@
-import { inflections } from 'inflected';
+// Model
+export { defineToken, defineModel } from './model';
+export type {
+  InferTokenCollectionName,
+  InferTokenId,
+  InferTokenMeta,
+  InferTokenModel,
+  InferTokenModelName,
+  InferTokenSerialized,
+  ModelAttrs,
+  ModelClass,
+  ModelInstance,
+  ModelToken,
+  NewModelAttrs,
+  NewModelInstance,
+  PartialModelAttrs,
+} from './model';
 
-export { DB } from './db';
-export { inflections };
+// Factory
+export { createFactory, extendFactory } from './factory';
+export type {
+  Factory,
+  FactoryAttrs,
+  FactoryConfig,
+  FactoryDefinition,
+  TraitMap,
+  TraitName,
+} from './factory';
+
+// Schema
+export { setupSchema, createCollection, composeCollections } from './schema';
+export type {
+  CollectionModule,
+  SchemaCollectionConfig,
+  SchemaConfig,
+  SchemaInstance,
+} from './schema';
